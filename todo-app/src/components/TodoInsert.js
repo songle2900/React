79beac7946caf98444b7484f887539d2c1f7de0a
Young Min Song <ysong88@myseneca.ca>
2019-11-25 17:11:42 -0500

    Add object to todos array

--- todo-app/src/components/TodoInsert.js
+++ todo-app/src/components/TodoInsert.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
 const TodoInsert = () => {
+    // declare value for managing input
+    const [value, setValue] = useState('');
+
+    // use useCallback hook for reuseable function
+    const onChange = useCallback(e => {
+        setValue(e.target.value);
+    }, []);
+
     return (
         <form className="TodoInsert">
-            <input placeholder="Enter here" />
+            <input 
+                placeholder="Enter here"
+                value={value}
+                onChange={onChange}
+            />
             <button type="submit">
                 <MdAdd />
             </button>
