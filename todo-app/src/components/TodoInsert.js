fb10e7eef1851af33488853ee8fd0a0656e15f63
Young Min Song <ysong88@myseneca.ca>
2019-11-25 18:49:42 -0500

    onSubmit function added

--- todo-app/src/components/TodoInsert.js
+++ todo-app/src/components/TodoInsert.js
@@ -2,7 +2,7 @@ import React, { useState, useCallback } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
     // declare value for managing input
     const [value, setValue] = useState('');
 
@@ -11,8 +11,20 @@ const TodoInsert = () => {
         setValue(e.target.value);
     }, []);
 
+    const onSubmit = useCallback(
+        e => {
+            onInsert(value);
+            setValue(''); // set value as empty
+
+            // submit event occurs refresh on browser
+            // to prevent, call preventDefault();
+            e.preventDefault();
+        },
+        [onInsert, value],
+    );
+
     return (
-        <form className="TodoInsert">
+        <form className="TodoInsert" onSubmit={onSubmit}>
             <input 
                 placeholder="Enter here"
                 value={value}
