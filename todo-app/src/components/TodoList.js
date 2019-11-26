46deef972ce154d267750bae6bd740b08bb90fc6
Young Min Song <ysong88@myseneca.ca>
2019-11-25 19:22:18 -0500

    Add onRemove

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -2,12 +2,12 @@ import React from 'react';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, onRemove }) => {
     return (
         <div className="TodoList">
             {todos.map(todo => (
                 // key props is necessary when using map function
-                <TodoListItem todo={todo} key={todo.id} />
+                <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} />
             ))}
         </div>
     );
