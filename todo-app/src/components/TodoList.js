4a476bd8d3d7871cbf718dc12f1e5979e7dde5e1
Young Min Song <ysong88@myseneca.ca>
2019-11-26 13:16:40 -0500

    Add onToggle function and final update

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -2,12 +2,17 @@ import React from 'react';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
-const TodoList = ({ todos, onRemove }) => {
+const TodoList = ({ todos, onRemove, onToggle }) => {
     return (
         <div className="TodoList">
             {todos.map(todo => (
                 // key props is necessary when using map function
-                <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} />
+                <TodoListItem 
+                    todo={todo} 
+                    key={todo.id} 
+                    onRemove={onRemove} 
+                    onToggle={onToggle}
+                />
             ))}
         </div>
     );
