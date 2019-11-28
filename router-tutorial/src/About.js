66017ea8404112e9f9be08efdd42221fe80d8111
Young Min Song <ysong88@myseneca.ca>
2019-11-28 14:39:12 -0500

    React router practice

---
+++ router-tutorial/src/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>Intro</h1>
+            <p>Testing react router</p>
+        </div>
+    );
+};
+
+export default About;
