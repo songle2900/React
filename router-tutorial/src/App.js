ab1cd8affe0d847c8807548898de3d5de29279b3
Young Min Song <ysong88@myseneca.ca>
2019-11-29 15:36:18 -0500

    Switch Component

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
 import Profiles from './Profiles';
@@ -15,10 +15,20 @@ const App = () => {
         <li><Link to="/history">History example</Link></li>
       </ul>
       <hr />
+      <Switch>
       <Route path="/" component={Home} exact={true} />
       <Route path={['/about', '/info']} component={About} />
       <Route path="/profiles" component={Profiles} />
       <Route path="/history" component={HistorySample} />
+      <Route
+        render={({ location }) => (
+          <div>
+            <h2>This page does not exist:</h2>
+            <p>{location.pathname}</p>
+          </div>
+        )}
+      />
+      </Switch>
     </div>
   );
 };
