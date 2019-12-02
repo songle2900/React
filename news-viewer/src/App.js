0f10e2ef4a00f7d1d43743948f81ad6181ff8ef0
Young Min Song <ysong88@myseneca.ca>
2019-12-02 15:30:16 -0500

    news-viewer API

---
+++ news-viewer/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const App = () => {
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+      const response = await axios.get('https://newsapi.org/v2/top-headlines?country=ca&apiKey=a9f7c224f8b14c3cba5e003e0c3dbfe8');
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>Load</button>
+      </div>
+      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
+    </div>
+  );
+};
+
+export default App;
