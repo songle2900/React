faf46230a1253665c3de966e9d1b0b7636957548
Young Min Song <ysong88@myseneca.ca>
2020-01-25 14:51:28 -0500

    Updating sass-node

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -3,37 +3,37 @@ import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
-function createBulkTodos() {
-  const array = [];
-  for (let i = 1; i <= 2500; i++) {
-    array.push({
-      id: i,
-      text: `To do ${i}`,
-      checked: false,
-    });
-  }
-  return array;
-}
+// function createBulkTodos() {
+//   const array = [];
+//   for (let i = 1; i <= 2500; i++) {
+//     array.push({
+//       id: i,
+//       text: `To do ${i}`,
+//       checked: false,
+//     });
+//   }
+//   return array;
+// }
 
 const App = () => {
-  const [todos, setTodos] = useState(createBulkTodos);
-  // const [todos, setTodos] = useState([
-  //   {
-  //     id: 1,
-  //     text: 'Basic concepts of React',
-  //     checked: true,
-  //   },
-  //   {
-  //     id: 2,
-  //     text: 'Styling for Component',
-  //     checked: true,
-  //   },
-  //   {
-  //     id: 3,
-  //     text: 'Creating Agenda App',
-  //     checked: false,
-  //   },
-  // ]);
+  // const [todos, setTodos] = useState(createBulkTodos);
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: 'Basic concepts of React',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: 'Styling for Component',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: 'Creating Agenda App',
+      checked: false,
+    },
+  ]);
 
   // id for each value
   const nextId = useRef(2501);
