8ba6a72a4a358e5cd50b0df99f0f15042d623d46
Young Min Song <ysong88@myseneca.ca>
2020-02-13 15:13:17 -0500

    Update study files

--- ReactStudy/src/App.js
+++ ReactStudy/src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
+
 import classes from "./App.css";
 import Person from "./Person/Person";
+import ErrorBoundary from "./ErrorBoundary/ErrorBoundary";
 
 class App extends Component {
   state = {
     persons: [
-      { id: "E", name: "Eric", age: 1 },
-      { id: "C", name: "Carol", age: 2 },
-      { id: "D", name: "Danny", age: 3 }
+      { id: "asfa1", name: "Max", age: 28 },
+      { id: "vasdf1", name: "Manu", age: 29 },
+      { id: "asdf11", name: "Stephanie", age: 26 }
     ],
     otherState: "some other value",
     showPersons: false
   };
 
-  nameChangeHandler = (e, id) => {
+  nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
       return p.id === id;
     });
@@ -21,9 +23,10 @@ class App extends Component {
     const person = {
       ...this.state.persons[personIndex]
     };
-    // const person = Object.assign({}, this.state.person[personIndex]);
 
-    person.name = e.target.value;
+    // const person = Object.assign({}, this.state.persons[personIndex]);
+
+    person.name = event.target.value;
 
     const persons = [...this.state.persons];
     persons[personIndex] = person;
@@ -45,20 +48,21 @@ class App extends Component {
 
   render() {
     let persons = null;
-    let btnClass = [classes.button];
+    let btnClass = "";
 
     if (this.state.showPersons) {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
             return (
-              <Person
-                click={() => this.deletePersonHandler(index)}
-                name={person.name}
-                age={person.age}
-                key={person.id}
-                changed={e => this.nameChangeHandler(e, person.id)}
-              />
+              <ErrorBoundary key={person.id}>
+                <Person
+                  click={() => this.deletePersonHandler(index)}
+                  name={person.name}
+                  age={person.age}
+                  changed={event => this.nameChangedHandler(event, person.id)}
+                />
+              </ErrorBoundary>
             );
           })}
         </div>
@@ -67,14 +71,10 @@ class App extends Component {
       btnClass = classes.Red;
     }
 
-    // Setting className dynamically
-    // let classes = ['red', 'bold'].join(' '); // red bold
     const assignedClasses = [];
-
     if (this.state.persons.length <= 2) {
       assignedClasses.push(classes.red); // classes = ['red']
     }
-
     if (this.state.persons.length <= 1) {
       assignedClasses.push(classes.bold); // classes = ['red', 'bold']
     }
@@ -82,13 +82,14 @@ class App extends Component {
     return (
       <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(" ")}>This is really working!</p>
+        <p className={assignedClasses.join(" ")}>This is really working!</p>
         <button className={btnClass} onClick={this.togglePersonsHandler}>
           Toggle Persons
         </button>
         {persons}
       </div>
     );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
--- ReactStudy/src/Person/Person.js
+++ ReactStudy/src/Person/Person.js
@@ -1,30 +1,23 @@
-import React from 'react';
-import styled from 'styled-components';
-// import './Person.css';
-// import Radium from 'radium';
+import React from "react";
 
-const StyledDiv = styled.div `
-    width: 60%;
-    margin: 16px auto;
-    border: 1px solid #eee;
-    box-shadow: 0 2px 3px #ccc;
-    padding: 16px;
-    text-align: center;
+import classes from "./Person.css";
 
-    @media (min-width: 500px) {
-        width: 450px;                
-    }
-`;
+const person = props => {
+  const rnd = Math.random();
 
-const Person = ( props ) => {
-    return (
-        // <div className="Person" style={style}>
-        <StyledDiv>
-            <p onClick={props.click}>My name is {props.name} and I am {props.age} years old!!</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={props.name} />
-        </StyledDiv>
-    );
+  if (rnd > 0.7) {
+    throw new Error("Something went wrong");
+  };
+  
+  return (
+    <div className={classes.Person}>
+      <p onClick={props.click}>
+        I'm {props.name} and I am {props.age} years old!
+      </p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
+    </div>
+  );
 };
 
-export default Person;
+export default person;
